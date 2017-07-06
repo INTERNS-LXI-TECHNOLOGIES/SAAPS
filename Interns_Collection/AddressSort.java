5e6c5f6e867cc24768db8a56a2814d7e3f10a83a
sarangibalu <sarangibalu.a@lxisoft.com>
2017-07-06 16:58:52 +0530

    Exercise to implement collection
    Comparator and Comparable functions have been implemented

---
+++ Interns_Collection/AddressSort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class AddressSort implements Comparator<Intern>
+{
+	
+	public int compare(Intern intern1,Intern intern2)
+	{
+		return intern1.getAddress().compareTo(intern2.getAddress());
+	}
+}
