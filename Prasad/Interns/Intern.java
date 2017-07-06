db98ff279f770e7a0ea79e18dab615f4021779dd
psd <psd>
2017-07-06 17:02:21 +0530

    implementing comparator and comparable

--- Prasad/Interns/Intern.java
+++ Prasad/Interns/Intern.java
@@ -1,4 +1,6 @@
-public class Intern
+import java.util.*;
+
+public class Intern implements Comparable<Intern>
 {
 	private String name;
 	private String address;
@@ -9,14 +11,45 @@ public class Intern
 		this.name=name;
 		this.address=address;
 		this.ph_no=ph_no;
+
+	
 	}
+
+	public String toString()
+	{
+		return this.name + "\t\t" + this.address + "\t\t" + this.ph_no ;
+ 	}
+
+    public int compareTo(Intern intern1)
+    {
+       return this.name.compareTo(intern1.getName());
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+
+   public String getAddress()
+   {
+   	return this.address;
+   }
 
 	public void printDetails()
 	{
 	
-	System.out.println(" \t"+this.name+"  \t"+this.address+" \t\t"+this.ph_no+"  ");
+	System.out.println(" \t"+this.name+"\t\t"+this.address+"\t\t"+this.ph_no+"  ");
 	
-
 		 
 	}
-}
+/*
+	public void sortByName()
+	{
+		for(int i=0;i<10;i++)
+		{
+			Arrays.sort(this.name);
+		}
+		
+	} */
+} 
--- Prasad/Interns/Tdd.java
+++ Prasad/Interns/Tdd.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Tdd
 {
 	public static void main(String[] args)
@@ -5,24 +6,71 @@ public class Tdd
 		Intern[] interns=new Intern[10];
 
 
-		interns[0] = new Intern("aaa","tcr","123456789");
-		interns[1] = new Intern("bbb","wky","135792468");
-		interns[2] = new Intern("ccc","knr","987654321");
-		interns[3] = new Intern("ddd","kzkd","975318642");
-		interns[4] = new Intern("eee","kkr","112233445");
-		interns[5] = new Intern("fff","rr","556677889");
-		interns[6] = new Intern("ggg","plkd","990011223");
-		interns[7] = new Intern("hhh","ktr","123453124");
-		interns[8] = new Intern("iii","knr","996688554");
-		interns[9] = new Intern("jjj","ttr","999897965");
+		interns[0] = new Intern("Zak","London","123456789");
+		interns[1] = new Intern("Sam","Italy","135792468");
+		interns[2] = new Intern("Philip","Us","987654321");
+		interns[3] = new Intern("James","Spain","975318642");
+		interns[4] = new Intern("Prasad","India","112233445");
+		interns[5] = new Intern("Rose","NewYork","556677889");
+		interns[6] = new Intern("Dayana","Paris","990011223");
+		interns[7] = new Intern("Jack","Austria","123453124");
+		interns[8] = new Intern("Kareena","Newland","996688554");
+		interns[9] = new Intern("Teena","England","999897965");
+
+
+SortByAddress address = new SortByAddress();
+		
+
+/*
+		for(int i=0;i<interns.length;i++)
+		{
+			System.out.print(sList.get(i)+"\n");
+		}*/
 
 		System.out.println("\n\t..........................Details of interns are.........................................");
 
-		System.out.println("\n\n\t Name \t Address\tPhone\n\n");
+		System.out.println("\n\n\t Name \t\tAddress\t\tPhone\n\n");
 
 		for(int i=0;i<interns.length;i++)
 		{
            interns[i].printDetails();
+
+           /*Arrays.sort(interns[i].sortByName());*/
 		}
+
+
+
+		List <Intern> internList = Arrays.asList(interns);
+          	
+	
+
+		System.out.println("\n\t...............Array converted To List............................... \n");
+
+		for(Intern details: internList)
+
+			System.out.print("\n\t"+ details );
+
+		System.out.println("\n"); 
+
+             /*details.sortByName();*/
+
+        Collections.sort(internList);
+
+		System.out.println("\n\t..........Sorted by Name using Comparable.............................\n");
+
+		for(Intern details : internList)
+
+		System.out.println("\n\t"+details);
+		
+
+     Collections.sort(internList,address);
+
+     System.out.print("\n\n\t...........Sorted by Address using Comarator..............................\n");
+
+     for(Intern details : internList)
+
+     	System.out.println("\n\t"+details);
+
+
 	}
 }
